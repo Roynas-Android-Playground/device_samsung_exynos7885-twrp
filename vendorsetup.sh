6b3c1ce89917e3b8a51b77e7620688c2692a4076
Soo-Hwan Na <whiteshell2544@naver.com>
2024-10-06 14:27:19 +0900

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d prebuilts/clang/host/linux-x86/clang-weeb ]; then
-    echo "Downloading WeebX-Clang..."
-    wget "$(curl -s https://raw.githubusercontent.com/XSans0/WeebX-Clang/main/main/link.txt)" -O "weebx-clang.tar.gz"
-    mkdir prebuilts/clang/linux-x86/clang-weeb
-    tar -xvf weebx-clang.tar.gz -C prebuilts/clang/linux-x86/clang-weeb && rm -rf weebx-clang.tar.gz
-fi
